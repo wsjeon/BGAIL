bb75386633a190074f088fa9ad12f8af1cde1c4e
Wonseok Jeon <wonsjeon@kaist.ac.kr>
2018-08-30 13:23:18 +0900

    Modify run to erase logging (on-going).

--- run.py
+++ run.py
@@ -1,8 +1,16 @@
 import baselines.run as run
 import tensorflow as tf
+import numpy as np
+import os.path as osp
 import gym
-from baselines.common.cmd_util import arg_parser
+from baselines.common.cmd_util import arg_parser, parse_unknown_args
 from baselines.common.tf_util import get_session
+from baselines.run import parse
+from baselines import logger
+try:
+    from mpi4py import MPI
+except ImportError:
+    MPI = None
 
 
 def build_env(args):
@@ -33,7 +41,6 @@ def common_arg_parser():
     parser.add_argument('--num_env', help='Number of environment copies being run in parallel. When not specified, set to number of cpus for Atari, and to 1 for Mujoco', default=None, type=int)
     parser.add_argument('--reward_scale', help='Reward scale factor. Default: 1.0', default=1.0, type=float)
     parser.add_argument('--save_path', help='Path to save trained model to', default=None, type=str)
-    parser.add_argument('--play', default=False, action='store_true')
 
     return parser
 
@@ -72,6 +79,26 @@ def train(args, extra_args):
     )
 
     return model, env
+
+
+def main():
+    # configure logger, disable logging in child MPI processes (with rank > 0)
+
+    arg_parser = common_arg_parser()
+    args, unknown_args = arg_parser.parse_known_args()
+    extra_args = {k: parse(v) for k,v in parse_unknown_args(unknown_args).items()}
+
+
+    if MPI is None or MPI.COMM_WORLD.Get_rank() == 0:
+        rank = 0
+    else:
+        rank = MPI.COMM_WORLD.Get_rank()
+
+    model, _ = train(args, extra_args)
+
+    if args.save_path is not None and rank == 0:
+        save_path = osp.expanduser(args.save_path)
+        model.save(save_path)
 
 
 if __name__ == '__main__':
