8ebe4789f023152c74407f2189ac27fd75caf913
Wonseok Jeon <jeonwonseok0125@gmail.com>
2018-08-30 22:24:01 +0900

    Clean up `run.py`.

--- run.py
+++ run.py
@@ -1,32 +1,13 @@
 import baselines.run as run
-import tensorflow as tf
-import numpy as np
-import os.path as osp
 import gym
 from baselines.common.cmd_util import arg_parser, parse_unknown_args
-from baselines.common.tf_util import get_session
-from baselines.run import parse
-from baselines import logger
-try:
-    from mpi4py import MPI
-except ImportError:
-    MPI = None
 
 
 def build_env(args):
     env_type, env_id = run.get_env_type(args.env)
-    if env_type == 'mujoco':
-        get_session(tf.ConfigProto(allow_soft_placement=True,
-                                   intra_op_parallelism_threads=1,
-                                   inter_op_parallelism_threads=1))
-
+    if env_type in ['mujoco', 'classic_control']:
         env = gym.make(env_id)
         env.seed(args.seed)
-
-    elif env_type == 'classic_control':
-        env = gym.make(env_id)
-        env.seed(args.seed)
-
     else:
         raise NotImplementedError
 
@@ -35,10 +16,9 @@ def build_env(args):
 
 def common_arg_parser():
     parser = arg_parser()
-    parser.add_argument('--env', help='environment ID', type=str, default='Walker2d-v1')
+    parser.add_argument('--env', help='environment ID', type=str, default='Hopper-v1')
     parser.add_argument('--seed', help='RNG seed', type=int, default=0)
     parser.add_argument('--alg', help='Algorithm', type=str, default='bgail')
-    parser.add_argument('--num_env', help='Number of environment copies being run in parallel. When not specified, set to number of cpus for Atari, and to 1 for Mujoco', default=None, type=int)
     parser.add_argument('--reward_scale', help='Reward scale factor. Default: 1.0', default=1.0, type=float)
     parser.add_argument('--save_path', help='Path to save trained model to', default='./outputs', type=str)
 
@@ -54,9 +34,6 @@ def get_alg_module(alg, submodule=None):
 
 def train(args, extra_args):
     env_type, env_id = run.get_env_type(args.env)
-    # TODO: either 'mujoco' or 'classic_control'
-    if env_type not in ['mujoco']:
-        raise NotImplementedError
 
     if args.alg is 'gail':
         env_type += '_gail'
@@ -68,7 +45,7 @@ def train(args, extra_args):
     alg_kwargs = run.get_learn_function_defaults(args.alg, env_type)
     alg_kwargs.update(extra_args)
 
-    env = run.build_env(args)
+    env = build_env(args)
 
     print('Training {} on {}:{} with arguments \n{}'.format(args.alg, env_type, env_id, alg_kwargs))
 
@@ -76,28 +53,13 @@ def train(args, extra_args):
 
 
 def main():
-    # configure logger, disable logging in child MPI processes (with rank > 0)
-
     arg_parser = common_arg_parser()
     args, unknown_args = arg_parser.parse_known_args()
-    extra_args = {k: parse(v) for k,v in parse_unknown_args(unknown_args).items()}
+    extra_args = {k: run.parse(v) for k,v in parse_unknown_args(unknown_args).items()}
 
-
-    if MPI is None or MPI.COMM_WORLD.Get_rank() == 0:
-        rank = 0
-    else:
-        rank = MPI.COMM_WORLD.Get_rank()
-
-    model, _ = train(args, extra_args)
-
-    if args.save_path is not None and rank == 0:
-        save_path = osp.expanduser(args.save_path)
-        model.save(save_path)
+    train(args, extra_args)
 
 
 if __name__ == '__main__':
-    run.build_env = build_env
     run.get_alg_module = get_alg_module
-    run.train = train
-    run.common_arg_parser = common_arg_parser
-    run.main()
+    main()
