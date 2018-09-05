1c9e33fde7de10ac9275ba85af8fc6f9b106fd47
Wonseok Jeon <jeonwonseok0125@gmail.com>
2018-09-05 22:19:13 +0900

    Prevent the generation of `__pycache__` and `.pyc`.

--- train.sh
+++ train.sh
@@ -2,4 +2,4 @@
 source /home/wsjeon/.bashrc
 source /home/wsjeon/.profile
 cd $2
-/home/wsjeon/anaconda3/envs/bgail/bin/python train_multiple_runs.py --process_id $1
+PYTHONDONTWRITEBYTECODE=1 /home/wsjeon/anaconda3/envs/bgail/bin/python -B train_multiple_runs.py --process_id $1
